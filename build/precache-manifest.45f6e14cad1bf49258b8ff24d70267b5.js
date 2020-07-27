83cdd34b4b793fa13278291ba43dae0b043b3cd2
essipurh <purhonen.essi@gmail.com>
2020-07-27 15:00:16 +0300

    ui heroku updates

---
+++ build/precache-manifest.45f6e14cad1bf49258b8ff24d70267b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40560cc632f6e4f2b405a12d95987f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd3f6db286f97a45bb6b",
+    "url": "/static/css/main.d2910d7f.chunk.css"
+  },
+  {
+    "revision": "bade6badc4ee71057448",
+    "url": "/static/js/2.483051de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.483051de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd3f6db286f97a45bb6b",
+    "url": "/static/js/main.3dffb6f9.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bf90ddfd72f8a2c95a510c06740eed7.js"
+  "/precache-manifest.45f6e14cad1bf49258b8ff24d70267b5.js"
 );
 
 self.addEventListener('message', (event) => {
