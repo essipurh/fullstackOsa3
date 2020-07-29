b93df5eb7f9b9df0736a54624bf08a4c15070d9e
essipurh <purhonen.essi@gmail.com>
2020-07-29 20:00:35 +0300

    fiksaus updateen heroku

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ app.put('/api/persons/:id', (req, res, next) => {
       number: body.number,
     }
   
-    Person.findByIdAndUpdate(req.params.id, person, { new: true }, { runValidators: true, context: 'query' })
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
       .then(updatedPerson => {
         res.json(updatedPerson.toJSON())
       })
